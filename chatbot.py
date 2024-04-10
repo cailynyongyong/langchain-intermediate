64a2330753a99795e9e3cf092c61ee5beb0be974
Cailyn Yong <cailynyongyong@gmail.com>
2024-04-11 02:16:57 +0900

    first commit

---
+++ chatbot.py
@@ -0,0 +1,123 @@
+from dotenv import load_dotenv
+load_dotenv()
+import openai
+import os
+openai.api_key= os.environ.get("OPENAI_API_KEY")
+import streamlit as st
+import time
+from langchain_openai import ChatOpenAI
+
+chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
+
+from langchain_community.document_loaders import WebBaseLoader
+
+loader = WebBaseLoader("https://dalpha.so/ko/howtouse")
+data = loader.load()
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+
+# k is the number of chunks to retrieve
+retriever = vectorstore.as_retriever(k=4)
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+from langchain_core.runnables import RunnablePassthrough
+
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    chat, retriever, contextualize_q_prompt
+)
+
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(chat, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+# 웹사이트 제목
+st.title("AI Chatbot")
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+        
+# Define a threshold for the maximum number of messages before deletion occurs
+MAX_MESSAGES_BEFORE_DELETION = 4
+
+# 웹사이트에서 유저의 인풋을 받고 위에서 만든 AI 에이전트 실행시켜서 답변 받기
+if prompt := st.chat_input("Dalpha AI store는 어떻게 사용하나요?"):
+    
+# 유저가 보낸 질문이면 유저 아이콘과 질문 보여주기
+     # Check if the number of messages exceeds the threshold
+    if len(st.session_state.messages) >= MAX_MESSAGES_BEFORE_DELETION:
+        # Remove the first two messages
+        del st.session_state.messages[0]
+        del st.session_state.messages[0]  # Note: This is not a mistake. After deleting the first element, the next element becomes the first.
+   
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+# AI가 보낸 답변이면 AI 아이콘이랑 LLM 실행시켜서 답변 받고 스트리밍해서 보여주기
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        result = rag_chain.invoke({"input": prompt, "chat_history": st.session_state.messages})
+
+        with st.expander("Evidence context"):
+            st.write(result["context"])
+
+        for chunk in result["answer"].split(" "):
+            full_response += chunk + " "
+            time.sleep(0.2)
+            message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+            
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+print("_______________________")
+print(st.session_state.messages)
